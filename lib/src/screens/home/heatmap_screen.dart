04fb755dfc33bdf6e188164c137f4303614092b8
kagwicharles <kagwitheuri@gmail.com>
2024-08-02 14:36:52 +0300

    update ui

--- lib/src/screens/home/heatmap_screen.dart
+++ lib/src/screens/home/heatmap_screen.dart
@@ -2,6 +2,8 @@ import 'package:ecosense/src/blocs/heatmap/heatmap_bloc.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../views/loading_view.dart';
+
 class HeatmapScreen extends StatefulWidget {
   const HeatmapScreen({super.key});
 
@@ -19,10 +21,18 @@ class _HeatmapScreenState extends State<HeatmapScreen> {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-        child: SingleChildScrollView(
-      child: Column(
-        children: [],
-      ),
-    ));
+        child: BlocListener<HeatMapBloc, HeatMapState>(
+            listener: (context, state) {},
+            child: BlocBuilder<HeatMapBloc, HeatMapState>(
+                builder: (context, state) {
+              if (state is HeatMapLoadingState) const LoadingView();
+              if (state is HeatMapLoadingState) {
+                return const LoadingView();
+              }
+              if (state is HeatMapLoadedState) {
+                return const Text("HeatMAP");
+              }
+              return const SizedBox();
+            })));
   }
 }
